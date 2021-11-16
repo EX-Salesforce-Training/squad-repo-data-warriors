98ff049da5b8b8ca9f9b993aa0ef08179f504ab4
Trevor Colton <trevor.colton@revature.net>
2021-11-16 14:23:47 -0700

    Cleaned up nav bar and css files

--- project-zero/src/client/modules/my/navCategories/navCategories.js
+++ project-zero/src/client/modules/my/navCategories/navCategories.js
@@ -2,11 +2,33 @@ import { LightningElement } from 'lwc';
 
 export default class NavCategories extends LightningElement {
     navItems = [
-        "Home",
-        "Snowboard",
-        "Ski",
-        "Brands",
-        "Accessories",
-        "Sales"
+        {
+            Id: '0',
+            Name: "Home"
+        },
+        {
+            Id: '1',
+            Name: "Snowboard"
+        },
+        {
+            Id: '2',
+            Name: "Skiis"
+        },
+        {
+            Id: '3',
+            Name: "Gear"
+        },
+        {
+            Id: '4',
+            Name: "Jackets"
+        },
+        {
+            Id: '5',
+            Name: "Gloves"
+        },
+        {
+            Id: '6',
+            Name: "Sales"
+        },
     ]
 }
