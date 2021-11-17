6dcb056a2f5ecec0fecbea9f3e2cec48c2c5001f
Deep Patel <patel.deep1217@outlook.com>
2021-11-17 12:29:43 -0500

    login, signup and footer added

--- project0/src/modules/my/banner/banner.js
+++ project0/src/modules/my/banner/banner.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
+import coursehtml from '../course/course.html';
+export default class Banner extends LightningElement {
 
-export default class Banner extends LightningElement {}
+}
--- project0/src/modules/my/navbar/navbar.js
+++ project0/src/modules/my/navbar/navbar.js
@@ -1,6 +1,47 @@
 
 import { LightningElement } from "lwc";
 
+
 export default class Navbar extends LightningElement{
+    showLoginColor = false;
+    signup = false;
+    login = false;
+    
+    showSignup(){
+        if(!this.login){
+            this.template.querySelector('.signup').style.backgroundColor ='#FF8A63';
+            this.signup = !this.signup;
+        }
+        if(this.signup == false){
+            this.template.querySelector('.signup').style.backgroundColor = "#fff3e5";
+        }
+        
+    }
+
+    closeSignup(){
+        this.template.querySelector('.signup').style.backgroundColor ='#fff3e5';
+        this.signup = false;
+    }
+
+   
+    showLogin(){
+
+        if(this.signup == false){
+
+            this.template.querySelector('.login').style.backgroundColor ='#FF8A63';
+            this.login = !this.login;
+            if(this.login == false){
+                this.template.querySelector('.login').style.backgroundColor = "#fff3e5";
+            }
+        }
+        
+    }
+    closeLogin(){
+      this.template.querySelector('.login').style.backgroundColor ='#fff3e5';
+        this.login = false;
+    }
+
+
+
     
 }
