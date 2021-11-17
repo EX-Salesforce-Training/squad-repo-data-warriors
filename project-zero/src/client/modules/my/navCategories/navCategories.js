d7745554be8e05438d44a863509bb6146abd9d5b
Trevor Colton <trevor.colton@revature.net>
2021-11-17 13:42:35 -0700

    Finished outline of main page and cleaned up the css/html

--- project-zero/src/client/modules/my/navCategories/navCategories.js
+++ project-zero/src/client/modules/my/navCategories/navCategories.js
@@ -1,34 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class NavCategories extends LightningElement {
-    navItems = [
-        {
-            Id: '0',
-            Name: "Home"
-        },
-        {
-            Id: '1',
-            Name: "Snowboard"
-        },
-        {
-            Id: '2',
-            Name: "Skiis"
-        },
-        {
-            Id: '3',
-            Name: "Gear"
-        },
-        {
-            Id: '4',
-            Name: "Jackets"
-        },
-        {
-            Id: '5',
-            Name: "Gloves"
-        },
-        {
-            Id: '6',
-            Name: "Sales"
-        },
+    navDropdowns = [
+        "Home", "Snowboards", "Skis", "Brands", "Accessories", "Sale"
     ]
 }
